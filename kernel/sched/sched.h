69362c9940c4142e2cc8615c48d727d3d3df536d
EmanuelCN <emanuelghub@gmail.com>
2023-10-19 01:48:24 +0700

    kernel: sched: Remove check_for_migration()
    This calls select_energy_cpu_brute() which CASS has an alternative and will take care of, so remove it we leave the job to CASS.
    
     Conflicts:
    	kernel/sched/sched.h
    
    Signed-off-by: Rsyd <rasyid2007alkautsar@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2753,7 +2753,6 @@ extern int alloc_related_thread_groups(void);
 
 extern unsigned long all_cluster_ids[];
 
-extern void check_for_migration(struct rq *rq, struct task_struct *p);
 
 static inline int is_reserved(int cpu)
 {
@@ -2853,8 +2852,6 @@ static inline bool task_sched_boost(struct task_struct *p)
 {
 	return true;
 }
-
-static inline void check_for_migration(struct rq *rq, struct task_struct *p) { }
 
 static inline bool hmp_capable(void) { return false; }
 static inline bool is_max_capacity_cpu(int cpu) { return true; }
